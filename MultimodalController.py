bb5f64cb961a3cf63371782e2d69e5b00063cc4b
mrgiolando <giolando.mark@gmail.com>
2021-05-05 01:41:08 -0700

    update

--- MultimodalController.py
+++ MultimodalController.py
@@ -6,6 +6,7 @@ try:
 except ImportError:
     print("This  computer  does  not  appear  to be a PiCar -X system(/opt/ezblock  is not  present). Shadowing  hardware  callswith  substitute  functions ")
     from  sim_ezblock  import *
+import sys
 import SenseClass as sc
 import ControllerClass as cc
 from InterpreterClass import InterpreterClass
@@ -15,6 +16,16 @@ import ultrasonicControllerClass as ucc
 from ultrasonicInterpreterClass import ultrasonicInterpreterClass
 import ultrasonicSenseClass as usc
 
+def test_sensor_function(sensorBus, delay):
+    while True:
+        # tmpMsg = [0, 0, 0]
+        # tmpMsg[0] = sc.SenseClass.S0()
+        # tmpMsg[1] = sc.SenseClass.S1()
+        # tmpMsg[2] = sc.SenseClass.S2()
+        s = sc.SenseClass()
+        tmpMsg = s.sensor_reading()
+        sensorBus.set_message(tmpMsg, 'sensor')
+        time.sleep(delay)
 
 def sensor_function(sensorBus, delay):
     while True:
@@ -44,8 +55,6 @@ def interpretation_function(sensorBus, interpreterBus, delay):
 
         #delay
         time.sleep(delay)
-
-
 
 
 def controller_function(interpreterBus, delay):
@@ -127,39 +136,68 @@ if __name__ == "__main__":
 
 
 
+
+
+
+
     #Line Follower
-    rosSensor1 = Producer
-    # output sensorBus
-    rosSensor1.__init__(sensor_function(sensor_value_bus, 0.1), sensor_value_bus)
-
-    rosInterpreter1 = ConsumerProducer
-    rosInterpreter1.__init__(interpretation_function(sensor_value_bus, direction_bus, 0.1), sensor_value_bus, direction_bus)
-    #input sensorBus
-    #output directionBus
-
-    rosController1 = Consumer
-    #input directionBus
-    rosController1.__init__(controller_function(direction_bus, 0.1), direction_bus)
+    # rosSensor1 = Producer('sensor_function', sensor_value_bus, 0.1, default_termination_bus, 'rosSensor1')
+    rosSensor1 = Producer('sensor_function', sensor_value_bus, 0.1, default_termination_bus, 'rosSensor1')
+    rosInterpreter1 = ConsumerProducer('interpretation_function', sensor_value_bus, direction_bus, 0.1, default_termination_bus, 'rosInterpreter1')
+    rosController1 = Consumer('controller_function', direction_bus, 0.1, default_termination_bus, 'rosController1')
 
 
     #Ultrasonic stop/go
     ultrasonic_value_bus = Bus()
     stopgo_bus = Bus()
+    rosSensor2 = Producer('ultrasensor_function', ultrasonic_value_bus, 0.1, default_termination_bus, 'rosSensor2')
+    rosInterpreter2 = ConsumerProducer('ultra_interpretation_function', ultrasonic_value_bus, stopgo_bus, 0.1, default_termination_bus, 'rosInterpreter2')
+    rosController2 = Consumer('ultra_controller_function', stopgo_bus, 0.1, default_termination_bus, 'rosController2')
 
-    rosSensor2 = Producer
-    # output sensorBus
-    rosSensor2.__init__(ultrasensor_function(ultrasonic_value_bus, 0.1), ultrasonic_value_bus)
-
-    rosInterpreter2 = ConsumerProducer
-    rosInterpreter2.__init__(ultra_interpretation_function(ultrasonic_value_bus, stopgo_bus, 0.1), ultrasonic_value_bus, stopgo_bus)
-    #input sensorBus
-    #output directionBus
-
-    rosController2 = Consumer
-    #input directionBus
-    rosController2.__init__(ultra_controller_function(stopgo_bus, 0.1), stopgo_bus)
+    # sys.exit()
+    #run
+    runConcurrently([rosSensor1, rosController1, rosInterpreter1, rosSensor1, rosController1, rosInterpreter1])
+    # runConcurrently([rosSensor1])
 
 
+
+    # sys.exit()
+    #
+    # #Line Follower
+    # rosSensor1 = Producer
+    # # output sensorBus
+    # # rosSensor1.__init__(sensor_function(sensor_value_bus, 0.1), sensor_value_bus)
+    #
+    # rosInterpreter1 = ConsumerProducer
+    # # rosInterpreter1.__init__(interpretation_function(sensor_value_bus, direction_bus, 0.1), sensor_value_bus, direction_bus)
+    # #input sensorBus
+    # #output directionBus
+    #
+    # rosController1 = Consumer
+    # #input directionBus
+    # # rosController1.__init__(controller_function(direction_bus, 0.1), direction_bus)
+    #
+    #
+    # #Ultrasonic stop/go
+    # ultrasonic_value_bus = Bus()
+    # stopgo_bus = Bus()
+    #
+    # rosSensor2 = Producer
+    # # output sensorBus
+    # # rosSensor2.__init__(ultrasensor_function(ultrasonic_value_bus, 0.1), ultrasonic_value_bus)
+    #
+    # rosInterpreter2 = ConsumerProducer
+    # # rosInterpreter2.__init__(ultra_interpretation_function(ultrasonic_value_bus, stopgo_bus, 0.1), ultrasonic_value_bus, stopgo_bus)
+    # #input sensorBus
+    # #output directionBus
+    #
+    # rosController2 = Consumer
+    # #input directionBus
+    # # rosController2.__init__(ultra_controller_function(stopgo_bus, 0.1), stopgo_bus)
+    #
+    #
+    # #run
+    # runConcurrently([rosSensor1, rosController1, rosInterpreter1, rosSensor1, rosController1, rosInterpreter1])
 
 
 
